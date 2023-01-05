5081224414c9d86a4187931de6c67c2dfefc8175
Ccl-viva <xujuanhan@gmail.com>
2023-01-05 22:26:08 +0800

    thins

---
+++ list.py
@@ -0,0 +1,28 @@
+# motocycles = ["honda","yamaha","suzuki"]
+# motocycles.append("chunfeng")
+# motocycles.insert(0,"chunfeng1")
+
+# del motocycles[0]
+
+# poped_motorcycles = motocycles.pop()
+# motocycles.remove("honda")
+
+# people_list = ["zhang","hu","lee"]
+# people_list.sorted()
+
+# magicians =["alice","david","carolina"]
+
+# for magician in magicians:
+#     print(magician)
+
+numbers = list(range(1,10))
+numbers2 = list(range(1,10,2))
+
+squares = []
+for value in range(1,10):
+    square = value **2
+    squares.append(square)
+
+
+
+
